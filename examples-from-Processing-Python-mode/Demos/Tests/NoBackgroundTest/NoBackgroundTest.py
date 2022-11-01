da8376355a36e940fbba80c4a192d8e9ef83bd3e
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2022-10-31 21:52:57 -0300

    Revert "Processing python mode examples (#2)"
    This reverts commit 45aeb58366102318ce91d4a0fdfbe7416bf94198.

--- examples-from-Processing-Python-mode/Demos/Tests/NoBackgroundTest/NoBackgroundTest.py
+++
@@ -1,8 +0,0 @@
-def setup():
-    size(400, 400, P2D)
-    background(255, 0, 0)
-    fill(255, 150)
-
-
-def draw():
-    ellipse(mouse_x, mouse_y, 100, 100)
--- examples-from-Processing-Python-mode/Topics/Geometry/Icosahedra/shape3d.py
+++
@@ -1,6 +0,0 @@
-class Shape3D(object):
-
-    def __init__(self, x=0, y=0, z=0):
-        self.location_x = x
-        self.location_y = y
-        self.location_z = z
